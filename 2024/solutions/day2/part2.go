be3faaac586463f9279c82926e83ae8ea4879769
Simon Gombrii <simon@gombrii.se>
2024-12-03 12:06:37 +0100

    Simplify slicing in day2 solution

--- 2024/solutions/day2/part2.go
+++ 2024/solutions/day2/part2.go
@@ -20,27 +20,17 @@ func Part2(file string) {
 }
 
 func safeWithDampening(report []int) bool {
-	tree := createTree(report)
-	for _, variant := range tree {
-		if safe(variant) {
+	fmt.Printf("Checking %v\n", report)
+	if safe(report) {
+		fmt.Printf("Report %v is safe\n", report)
+		return true
+	}
+	for i := range report {
+		if safe(append(report[:i:i], report[i+1:]...)) {
+			fmt.Printf("Report %v is safe with %d removed\n", report, report[i])
 			return true
 		}
 	}
+	fmt.Printf("Report %v is unsafe\n", report)
 	return false
-}
-
-func createTree(report []int) [][]int {
-	tree := make([][]int, len(report)+1)
-	tree[0] = report
-	for i := range report {
-		tree[i+1] = removeItem(report, i)
-	}
-	return tree
-}
-
-func removeItem(arr []int, index int) []int {
-	new := make([]int, len(arr)-1)
-	copy(new[:index], arr[:index])
-	copy(new[index:], arr[index+1:])
-	return new
 }
